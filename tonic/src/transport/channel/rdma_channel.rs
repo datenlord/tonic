3f0da4c30fd73fd1fdff940cc84e499eba3034f9
moui66744 <whaoyu09@163.com>
2022-11-25 03:06:36 -0500

    tonic: add simple RDMA support
    Server handles both HTTP and RDMA requests through `serve_with_rdma`.
    
    Client has two options to communicate with the server:
    either HTTP via Channel or RDMA via RdmaChannel.
    
    RDMA communication based on `async-rdma` crate.

---
+++ tonic/src/transport/channel/rdma_channel.rs
@@ -0,0 +1,78 @@
+use crate::{
+    body::BoxBody,
+    transport::{self, BoxFuture},
+};
+use async_rdma::{LocalMrReadAccess, LocalMrWriteAccess, Rdma};
+use http::{Request, Response};
+use http_body::Body;
+use std::{alloc::Layout, io, sync::Arc};
+use tokio::net::ToSocketAddrs;
+use tower::Service;
+
+/// A RDMA transport channel
+#[derive(Debug, Clone)]
+pub struct RdmaChannel {
+    rdma: Arc<Rdma>,
+}
+
+const MAX_MSG_LEN: usize = 10240;
+
+impl RdmaChannel {
+    /// Create a new [`RdmaChannel`]
+    pub async fn new<A>(addr: A) -> Result<Self, io::Error>
+    where
+        A: ToSocketAddrs,
+    {
+        // TODO: expose these params to user (create a `RdmaEndpoint` maybe)
+        let rdma = Arc::new(Rdma::connect(addr, 1, 1, MAX_MSG_LEN).await?);
+        Ok(Self { rdma })
+    }
+}
+
+impl Service<Request<BoxBody>> for RdmaChannel {
+    type Response = Response<hyper::Body>;
+    type Error = transport::Error;
+    type Future = BoxFuture<Self::Response, Self::Error>;
+    fn poll_ready(
+        &mut self,
+        _cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), Self::Error>> {
+        std::task::Poll::Ready(Ok(()))
+    }
+
+    // TODO: handle error
+    fn call(&mut self, req: Request<BoxBody>) -> Self::Future {
+        let rdma = Arc::clone(&self.rdma);
+        Box::pin(async move {
+            tokio::spawn(rdma_send_req(Arc::clone(&rdma), req));
+
+            let (resp_mr, len) = rdma.receive_with_imm().await.unwrap();
+            let len = len.unwrap() as usize;
+            let resp_vec = resp_mr.as_slice()[0..len].to_vec();
+
+            let body = hyper::Body::from(resp_vec);
+            let resp = http::Response::builder().body(body).unwrap();
+            Ok(resp)
+        })
+    }
+}
+
+async fn rdma_send_req(rdma: Arc<Rdma>, mut req: Request<BoxBody>) {
+    let mut req_header = req.uri().to_string().into_bytes();
+    req_header.push(32u8);
+    let len_header = req_header.len();
+    let mut req_mr = rdma
+        .alloc_local_mr(Layout::new::<[u8; MAX_MSG_LEN]>())
+        .unwrap();
+    req_mr.as_mut_slice()[0..len_header].copy_from_slice(req_header.as_slice());
+
+    let mut len = len_header;
+    while let Some(Ok(bytes)) = req.data().await {
+        let req_body = bytes.to_vec();
+        let len_body = req_body.len();
+        assert!(len + len_body <= MAX_MSG_LEN); // TODO: len > MAX_MSG_LEN
+        req_mr.as_mut_slice()[len..len + len_body].copy_from_slice(req_body.as_slice());
+        len += len_body;
+    }
+    rdma.send_with_imm(&req_mr, len as u32).await.unwrap();
+}
--- tonic/src/transport/server/mod.rs
+++ tonic/src/transport/server/mod.rs
@@ -35,6 +35,7 @@ use crate::transport::Error;
 use self::recover_error::RecoverError;
 use super::service::{GrpcTimeout, ServerIo};
 use crate::body::BoxBody;
+use async_rdma::{LocalMrReadAccess, LocalMrWriteAccess, Rdma, RdmaBuilder};
 use bytes::Bytes;
 use futures_core::Stream;
 use futures_util::{future, ready};
@@ -43,9 +44,11 @@ use http_body::Body as _;
 use hyper::{server::accept, Body};
 use pin_project::pin_project;
 use std::{
+    alloc::Layout,
     convert::Infallible,
     fmt,
     future::Future,
+    io,
     marker::PhantomData,
     net::SocketAddr,
     pin::Pin,
@@ -596,6 +599,35 @@ impl<L> Router<L> {
             .await
     }
 
+    /// Listening for HTTP requests from addr 
+    /// while listening for RDMA requests from rdma_addr
+    pub async fn serve_with_rdma<ResBody>(
+        self,
+        addr: SocketAddr,
+        rdma_addr: SocketAddr,
+    ) -> Result<(), super::Error>
+    where
+        L: Layer<Routes>,
+        L::Service: Service<Request<Body>, Response = Response<ResBody>> + Clone + Send + 'static,
+        <<L as Layer<Routes>>::Service as Service<Request<Body>>>::Future: Send + 'static,
+        <<L as Layer<Routes>>::Service as Service<Request<Body>>>::Error: Into<crate::Error> + Send,
+        ResBody: http_body::Body<Data = Bytes> + Send + 'static,
+        ResBody::Error: Into<crate::Error>,
+    {
+        // let routes_clone = self.routes.clone();
+        tokio::spawn(rdma_serve(rdma_addr, self.routes.clone()));
+
+        let incoming = TcpIncoming::new(addr, self.server.tcp_nodelay, self.server.tcp_keepalive)
+            .map_err(super::Error::from_source)?;
+        self.server
+            .serve_with_shutdown::<_, _, future::Ready<()>, _, _, ResBody>(
+                self.routes,
+                incoming,
+                None,
+            )
+            .await
+    }
+
     /// Consume this [`Server`] creating a future that will execute the server
     /// on [tokio]'s default executor. And shutdown when the provided signal
     /// is received.
@@ -845,4 +877,56 @@ where
 
         future::ready(Ok(svc))
     }
+}
+
+// TODO: handle this error
+async fn rdma_serve(addr: SocketAddr, routes: Routes) -> Result<(), io::Error> {
+    let rdma = RdmaBuilder::default()
+        .set_max_message_length(MAX_MSG_LEN)
+        .listen(addr)
+        .await?;
+    let routes_clone = routes.clone();
+    rdma_serve_inner(&rdma, routes_clone).await?;
+    loop {
+        let rdma = rdma.listen().await?;
+        let routes = routes.clone();
+        tokio::spawn(async move { rdma_serve_inner(&rdma, routes).await });
+    }
+}
+
+const MAX_MSG_LEN: usize = 10240;
+
+// TODO: handle this error
+async fn rdma_serve_inner(rdma: &Rdma, mut routes: Routes) -> Result<(), io::Error> {
+    println!("[server] connected!");
+
+    loop {
+        let (req_mr, len) = rdma.receive_with_imm().await?;
+        let len = len.unwrap() as usize;
+        let req_vec = req_mr.as_slice()[0..len].to_vec();
+
+        // deserialize
+        let idx = req_vec.iter().position(|num| *num == 32).unwrap();
+        let uri = &req_vec[0..idx];
+        let body = hyper::Body::from(req_vec[idx + 1..len].to_vec());
+        let req = http::Request::builder()
+            .method("POST")
+            .uri(uri)
+            .body(body)
+            .unwrap();
+
+        let mut resp = routes.call(req).await.unwrap();
+        let mut resp_mr = rdma
+            .alloc_local_mr(Layout::new::<[u8; MAX_MSG_LEN]>())
+            .unwrap();
+        let mut len = 0;
+        while let Some(Ok(bytes)) = resp.data().await {
+            let resp_body = bytes.to_vec();
+            let len_body = resp_body.len();
+            resp_mr.as_mut_slice()[len..len + len_body].copy_from_slice(resp_body.as_slice());
+            len += len_body;
+        }
+        rdma.send_with_imm(&resp_mr, len as u32).await.unwrap();
+    }
+    // Ok(())
 }
